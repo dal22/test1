a1255eb32266e2e1e4785b66e31cb171ef7110f5
Daljeet Sidhu <dalsidhu99@gmail.com>
2019-11-09 18:17:44 +0000

    first commit

---
+++ tests/testThree.js
@@ -0,0 +1,15 @@
+/**
+ * Created by daljeetsidhu on 09/11/2019.
+ */
+
+/**
+ * Created by daljeetsidhu on 09/11/2019.
+ */
+
+import { Selector } from 'testcafe';
+
+
+
+fixture `Getting Started 3rd fixture`
+    .page `http://www.google.com`;
+
